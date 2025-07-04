3b07dc690267290d700092fd562aa275daeaf894
mahdirnma <rahnamamahdi78@gmail.com>
2025-07-04 10:32:06 +0330

    create categories panel

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\TagController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -19,6 +21,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/admin', [UserController::class, 'dashboard'])->name('dashboard')/*->middleware('authorize')*/;
     Route::resource('posts', PostController::class);
     Route::patch('posts/{post}/publish', [PostController::class, 'publish'])->name('posts.publish');
+    Route::resource('categories', CategoryController::class);
+    Route::resource('tags', TagController::class);
+
 });
 Route::get('/login',[AuthController::class,'loginForm'])->name('login.form');
 Route::post('/login',[AuthController::class,'login'])->name('login');
