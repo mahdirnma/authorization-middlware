a0d554fd08bb8921e2f6a3dcbf11c5c5bbcb30bf
mahdirnma <rahnamamahdi78@gmail.com>
2025-07-04 10:39:15 +0330

    create tags panel

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -13,7 +13,8 @@ class TagController extends Controller
      */
     public function index()
     {
-        //
+        $tags = Tag::where('is_active', 1)->paginate(2);
+        return view('admin.tags.index', compact('tags'));
     }
 
     /**
@@ -21,7 +22,6 @@ class TagController extends Controller
      */
     public function create()
     {
-        //
     }
 
     /**
